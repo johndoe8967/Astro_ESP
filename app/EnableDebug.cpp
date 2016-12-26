7378c279a8cdd71eabe4a2c63c68ea8727e7b07f
John Doe <johndoe8967@gmx.at>
2016-12-26 12:51:41 +0100

    cleanup of startSPI and debug command

--- app/EnableDebug.cpp
+++ app/EnableDebug.cpp
@@ -17,7 +17,9 @@ EnableDebug::~EnableDebug()
 void EnableDebug::initCommand()
 {
 	commandHandler.registerCommand(CommandDelegate("enDebug","Enable Debug ","Appl",commandFunctionDelegate(&EnableDebug::processEnableDebug,this)));
+#ifdef startSPI
 	commandHandler.registerCommand(CommandDelegate("startSPI","Start SPI","Appl",commandFunctionDelegate(&EnableDebug::processStartSPI,this)));
+#endif
 	commandHandler.registerCommand(CommandDelegate("showIP","Show IP","Appl",commandFunctionDelegate(&EnableDebug::showIP,this)));
 }
 
@@ -28,6 +30,7 @@ void EnableDebug::showIP(String commandLine, CommandOutput* commandOutput) {
 	commandOutput->printf("\r\n");
 }
 
+#ifdef startSPI
 void EnableDebug::processStartSPI(String commandLine, CommandOutput* commandOutput)
 {
 	Vector<String> commandToken;
@@ -42,6 +45,8 @@ void EnableDebug::processStartSPI(String commandLine, CommandOutput* commandOutp
 	}
 
 }
+#endif
+
 void EnableDebug::processEnableDebug(String commandLine, CommandOutput* commandOutput)
 {
 	Vector<String> commandToken;
@@ -49,24 +54,20 @@ void EnableDebug::processEnableDebug(String commandLine, CommandOutput* commandO
 
 	if (numToken == 1)
 	{
-		commandOutput->printf("on/off/status\r\n");
+		commandOutput->printf("on/off\r\n");
 	}
 	else
 	{
+		commandOutput->printf("Status ");
 		if (commandToken[1] == "on")
 		{
 			status = true;
-			commandOutput->printf("Status ON\r\n");
+			commandOutput->printf("ON\r\n");
 		}
 		else if (commandToken[1] == "off")
 		{
 			status = false;
-			commandOutput->printf("Status OFF\r\n");
-		}
-		else if (commandToken[1] == "status")
-		{
-			String tempString = status ? "ON" : "OFF";
-			commandOutput->printf("Status is %s\r\n",tempString.c_str());
+			commandOutput->printf("OFF\r\n");
 		};
 		telnet.enableDebug(status);
 	}
